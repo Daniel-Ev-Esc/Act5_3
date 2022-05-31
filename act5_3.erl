1db1dfa4307f9a2a8bc2e318b69d79d639bd2f8d
Daniel-Esc <74694850+Daniel-Esc@users.noreply.github.com>
2022-05-31 12:48:54 -0500

    Sintaxis corregida
    Ya no tiene errores de sintaxis, pero no sé como probarlo

--- act5_3.erl
+++ act5_3.erl
@@ -1,6 +1,6 @@
 -module(act5_3).
--export([readlines/1, get_all_lines/1, run/1]).
-% -export([runParallel/1]).
+-export([readlines/1, get_all_lines/1, run/1, appendM/3, operations/3]).
+
 
 readlines(FileName) ->
     {ok, Device} = file:open(FileName, [read]),
@@ -14,29 +14,37 @@ get_all_lines(Device) ->
         Line -> Line ++ get_all_lines(Device)
     end.
 
+operations(N1,O,N2) -> if
+    O == '+' -> N1 + N2;
+    O == '-' -> N1 - N2;
+    O == '*' -> N1 * N2;
+    O == '/' -> N1 / N2;
+    O == '%' -> N1 rem N2
+end.
+
+
+
+appendM([H|T], I, N)-> if
+    I < (N + 1) ->
+        L0 = string:lexemes(H, " "),
+        N1 = list_to_integer(lists:nth(1, L0)),
+        Operador = list_to_integer(lists:nth(2, L0)),
+        N2 = list_to_integer(lists:nth(3, L0)),
+        
+        operations(N1, Operador, N2);
+
+    true -> appendM(T, I+1, N)
+end.
+
 
 run(FileName) ->
     L1 = readlines(FileName),
     L1,
     L2 = string:lexemes(L1, "\n"),
     L2,
-    L3 = string:lexemes(lists:nth(2, L2), " "),
-    % Con esto ya me separa el primer elemento de la lista en un arreglo de 3 strings,
-    % Pero ahora no sé cómo hacerlo iterativo y meterlo en la lista 
-    % para poder ya tener todos los arreglos de 3
-    L3.
-
-% runSequencial(File)->
-%     FileIn = File ++ "1.in",
-%     FileOut = File ++ "1.out",
-%     {ok, SIn} = file:open(FileIn, read),
-%     ListaLineas = get_all_lines(SIn),
-
-%     List1 = string:split(lists:nth(1,ListaLineas), " ",all),
-%     %List1,
-%     {ok, SOut} = file:open(FileOut, [write]),
-%     io:format(SOut, "~s~n", [List1]).
-
-
-
-
+    % L3 = string:lexemes(lists:nth(2, L2), " "),
+    % L3,
+    L2Len = length(L2),
+    L2Len,
+    ListOfList = appendM(L2, 1, L2Len),
+    ListOfList.
