04aaf50289dbbf607f1325195ed6c2e3ea39b8b0
geraldineecht <geraldineecht@gmail.com>
2022-05-31 11:22:26 -0500

    listas

--- act5_3.erl
+++ act5_3.erl
@@ -1,24 +1,37 @@
 -module(act5_3).
--export([runSequencial/1]).
--export([get_all_lines/1]).
--export([runParallel/1]).
+-export([readlines/1, get_all_lines/1, run/1]).
+% -export([runParallel/1]).
 
-get_all_lines(SIn) ->
-    case io:get_line(SIn, "") of
-        eof  -> [];
-        Line -> [Line] ++ get_all_lines(SIn)
+readlines(FileName) ->
+    {ok, Device} = file:open(FileName, [read]),
+    try get_all_lines(Device)
+      after file:close(Device)
     end.
 
-runSequencial(File)->
-    FileIn = File ++ "1.in",
-    FileOut = File ++ "1.out",
-    {ok, SIn} = file:open(FileIn, read),
-    ListaLineas = get_all_lines(SIn),
+get_all_lines(Device) ->
+    case io:get_line(Device, "") of
+        eof  -> [];
+        Line -> Line ++ get_all_lines(Device)
+    end.
 
-    List1 = string:split(lists:nth(1,ListaLineas), " ",all),
-    %List1,
-    {ok, SOut} = file:open(FileOut, [write]),
-    io:format(SOut, "~s~n", [List1]).
 
-runParallel(File)->
-    File.
+run(FileName) ->
+    L1 = readlines(FileName),
+    L1,
+    L2 = string:lexemes(L1, "\n"),
+    L2.
+
+% runSequencial(File)->
+%     FileIn = File ++ "1.in",
+%     FileOut = File ++ "1.out",
+%     {ok, SIn} = file:open(FileIn, read),
+%     ListaLineas = get_all_lines(SIn),
+
+%     List1 = string:split(lists:nth(1,ListaLineas), " ",all),
+%     %List1,
+%     {ok, SOut} = file:open(FileOut, [write]),
+%     io:format(SOut, "~s~n", [List1]).
+
+
+
+
