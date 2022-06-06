c3f228cab9e6e42cad624a8572200b5312062c3f
Carmina López <a00830265@itesm.mx>
2022-06-06 10:19:05 -0500

    CPP 6.1
    Creación de c++

---
+++ CPP/act5_2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main (){ 
+    string nombreArchivo;
+    cout << "System | Indique el nombre del archivo:" << endl;
+    cin >> nombreArchivo;
+
+    
+}
